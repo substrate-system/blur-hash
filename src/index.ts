365646586ae1eca386480b21d5b5c1c6a1cf60d7
Nick Thomas <nichoth@nichoth.com>
2024-07-12 01:22:11 -0700

    wip tests are ok

--- src/index.ts
+++ src/index.ts
@@ -16,18 +16,8 @@ export class BlurHash extends HTMLElement {
         const placeholder = this.getAttribute('placeholder')
         if (!placeholder) throw new Error('not placeholder')
 
-        debug('src', this.getAttribute('src'))
-
-        // this.innerHTML = `<img
-        //     ${srcset ? `srcset="${srcset}"` : ''}
-        //     alt="${this.getAttribute('alt')}"
-        //     content-visibility="${this.getAttribute('content-visibility') || 'auto'}"
-        //     decoding="${this.getAttribute('decoding') || 'async'}"
-        //     loading="${this.getAttribute('loading') || 'lazy'}"
-        //     class="image-element blurry"
-        //     style="background-size: cover; background-image: url('${placeholder}')"
-        //     src="${this.getAttribute('src')}"
-        // />`
+        document.body.style.setProperty('--blur-hash-time',
+            this.getAttribute('time') || '0.6s')
 
         this.innerHTML = `<canvas
             class="blurry"
@@ -41,42 +31,10 @@ export class BlurHash extends HTMLElement {
             decoding="${this.getAttribute('decoding') || 'async'}"
             loading="${this.getAttribute('loading') || 'lazy'}"
             class="image-element blurry"
-            style="background-size: cover; background-image: url('${placeholder}')"
             src="${this.getAttribute('src')}"
         />
         `
     }
-
-    // Define the attributes to observe
-    // need this for `attributeChangedCallback`
-    // static observedAttributes = ['exmaple']
-
-    // /**
-    //  * Handle [example] attribute changes
-    //  *
-    //  * @param  {string} oldValue The old attribute value
-    //  * @param  {string} newValue The new attribute value
-    //  */
-    // handleChange_example (oldValue:string, newValue:string) {
-    //     debug('handling change', oldValue, newValue)
-
-    //     if (newValue === null) {
-    //         // [example] was removed
-    //     } else {
-    //         // set [example] attribute
-    //     }
-    // }
-
-    // /**
-    //  * Runs when the value of an attribute is changed on the component
-    //  * @param  {string} name     The attribute name
-    //  * @param  {string} oldValue The old attribute value
-    //  * @param  {string} newValue The new attribute value
-    //  */
-    // attributeChangedCallback (name:string, oldValue:string, newValue:string) {
-    //     this[`handleChange_${name}`](oldValue, newValue)
-    //     debug('an attribute changed', name)
-    // }
 
     connectedCallback () {
         const width = parseInt(this.getAttribute('width') ?? '')
@@ -94,8 +52,6 @@ export class BlurHash extends HTMLElement {
         ctx.putImageData(imageData, 0, 0)
 
         const img = this.querySelector('img')!
-        debug('the image', img)
-        debug('img.complete', img.complete)
 
         img.addEventListener('load', () => {
             debug('loaded')
@@ -103,6 +59,8 @@ export class BlurHash extends HTMLElement {
             img.classList.remove('blurry')
             img.classList.add('sharp')
         })
+
+        debug('the image', img)
     }
 }
 
--- test/index.ts
+++ test/index.ts
@@ -6,11 +6,15 @@ test('example', async t => {
     document.body.innerHTML += `
         <blur-hash
             class="test"
-        >
-        </blur-hash>
+            width=30
+            height=30
+            src="/100.jpg"
+            placeholder="UHGIM_X900xC~XWFE0xt00o3%1oz-;t7i|IV"
+        ></blur-hash>
     `
 
-    const el = await waitFor('example-component')
+    const el = await waitFor('blur-hash')
 
-    t.ok(el, 'should find an element')
+    t.ok(el, 'should find the element')
+    t.ok(await waitFor('blur-hash canvas'), 'should contain a canvas')
 })
